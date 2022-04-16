9a60ea216103199f771b643e4f08fb5461b13175
Andrew Witwicki <vitvitskiy3@gmail.com>
2022-04-16 23:31:16 +0200

    Add karma decreasing

--- RudeBot/BotHandler.cs
+++ RudeBot/BotHandler.cs
@@ -245,5 +245,36 @@ namespace RudeBot
 
             await BotClient.TryDeleteMessage(msg);
         }
+
+        [MessageReaction(ChatAction.Typing)]
+        [MessageHandler("^-$")]
+        public async Task DecreaseKarma()
+        {
+            // OOh, look at here, is this code dUpLiCatIOn???
+            // Ignore message forwards
+            if (Message.ForwardFrom != null || Message.ForwardFromChat != null)
+                return;
+
+            // Filter only reply to other user, ignore bots
+            if (Message.ReplyToMessage == null || Message.ReplyToMessage.From.Id == User.Id || Message.ReplyToMessage.From.IsBot)
+                return;
+
+            UserChatStats userStats = await _userManager.GetUserChatStats(Message.ReplyToMessage.From.Id, ChatId);
+
+            // If user not exists in db then ignore
+            if (userStats == null)
+                return;
+
+            userStats.Karma--;
+            await _userManager.UpdateUserChatStats(userStats);
+
+            string replyText = $"Ви зменщили карму {userStats.User.UserName} до значення {userStats.Karma}!";
+
+            Message msg = await BotClient.SendTextMessageAsync(ChatId, replyText, replyToMessageId: Message.MessageId, parseMode: ParseMode.Markdown);
+
+            await Task.Delay(30 * 1000);
+
+            await BotClient.TryDeleteMessage(msg);
+        }
     }
 }
--- RudeBot/Consts.cs
+++ RudeBot/Consts.cs
@@ -8,7 +8,7 @@ namespace RudeBot
 {
     public static class Consts
     {
-        public const string BotVersion = "3.9.0";
+        public const string BotVersion = "3.10.0";
         public const string GithubUrl = "github.com/awitwicki/rude_bot/tree/dev_dotnet";
         public const string GoogleFormForNewbies = "https://forms.gle/pY6EjJhNRosUbd9P9";
         public const string WelcomeToTheClubBuddyVideoUrl = "https://github.com/awitwicki/rude_bot/blob/main/data/media/welcome.mp4?raw=true";

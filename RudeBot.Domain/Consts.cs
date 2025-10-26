4558a42a2c5564fa23d1f94a8476db1bfb8a90aa
Andrew Witwicki <vitvitskiy3@gmail.com>
2025-10-26 15:03:17 +0100

    Bump BotVersion to 3.40
    Updated the BotVersion constant from 3.39.4 to 3.40 to reflect the new version.

--- RudeBot.Domain/Consts.cs
+++ RudeBot.Domain/Consts.cs
@@ -2,7 +2,7 @@
 
 public static class Consts
 {
-    public const string BotVersion = "3.39.4";
+    public const string BotVersion = "3.40";
 
     public const string BadWordsService = "BadWordsService";
     public const string AdvicesService = "AdvicesService";

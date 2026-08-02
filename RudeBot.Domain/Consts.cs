7f9f1c1f6702b5571cdad88b7b9957ccc5d847bc
Andrew Witwicki <vitvitskiy3@gmail.com>
2026-08-02 09:32:34 +0200

    Gemini prompt fix

--- RudeBot.Domain/Consts.cs
+++ RudeBot.Domain/Consts.cs
@@ -2,7 +2,7 @@
 
 public static class Consts
 {
-    public const string BotVersion = "3.52.1";
+    public const string BotVersion = "3.52.2";
 
     public const string BadWordsService = "BadWordsService";
     public const string AdvicesService = "AdvicesService";
